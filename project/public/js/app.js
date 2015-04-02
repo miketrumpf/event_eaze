652e5dd94882e860b2976615c265fc532e73ee0a
Sara Morais <saracristinasm@gmail.com>
2015-04-02 09:34:24 -0400

    templates and bar views

--- project/public/js/app.js
+++ project/public/js/app.js
@@ -7,4 +7,9 @@ var App = {
 
 $(function() {
   console.log('Loaded, bro.');
+
+  App.bars = new App.Collections.Bars;
+  App.barsView = new App.Views.Bars({ collection: App.bars });
+  App.bars.fetch();
+
 });
--- project/public/js/models/bar.js
+++ project/public/js/models/bar.js
@@ -2,6 +2,7 @@ App.Models.Bar = Backbone.Model.extend({
 
   initialize: function() {
     console.log("Bar Models Created");
-  }
+  },
+  urlRoot: "/search_for_bars"
 
 });
