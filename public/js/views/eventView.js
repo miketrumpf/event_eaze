8be1cfffc1c221a03d7c5daded7aec4b1ca576db
Michael Trumpf <miketrumpf@gmail.com>
2015-04-03 15:52:34 -0400

    created single show view template to display when clicking show bar button to highlight show.  Can also click less info to switch back to previous view.

--- public/js/views/eventView.js
+++ public/js/views/eventView.js
@@ -6,6 +6,7 @@ App.Views.Event = Backbone.View.extend({
     //console.log("New Event view created")
     // this.listenTo(this.model, "change", this.render);
     this.eventTemplate = Handlebars.compile($("#event-list").html());
+    this.singleEventTemplate= Handlebars.compile($("#single-event").html());
     // $("#main").on("click", ".event", this.getFullEventInfo(this))
     this.render();
   },
@@ -19,14 +20,16 @@ App.Views.Event = Backbone.View.extend({
 
   events: {
     "click #show-bars": "seachForBars",
-    "click #more-info": "getFullEventInfo"
+    "click #more-info": "getFullEventInfo",
+    "click .less-info": "getLessEventInfo"
   },
 
   seachForBars: function() {
-    var event = this.model.attributes;
-    debugger
+    this.$el.empty();
+    var data = this.model.toJSON()
+    var compiledTemplate = this.singleEventTemplate(data);
+    this.$el.append(compiledTemplate)
 
-    // event.set({title: this.title, city_name: "this.city_name", start_time: 10, venue_name: "this.venue_name", venue_addess: "this.venue_address", description: "this.description", latitude: "this.latitude", longitude: "this.longitude"});
     App.bars.fetch();
 
   },
@@ -37,6 +40,14 @@ App.Views.Event = Backbone.View.extend({
     var newEventModalView = new App.Views.EventModal({model: this.model})
     
 
+  },
+
+  getLessEventInfo: function() {
+
+    this.$el.empty();
+    var data = this.model.toJSON();
+    var compiledTemplate = this.eventTemplate(data);
+    this.$el.append(compiledTemplate);
   }
 
 });
