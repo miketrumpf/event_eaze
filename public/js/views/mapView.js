d6f6c76ca3ff1f9e1f9c1e4f653068046e668421
Sara Morais <saracristinasm@gmail.com>
2015-04-07 10:12:10 -0400

    map

--- public/js/views/mapView.js
+++ public/js/views/mapView.js
@@ -5,7 +5,7 @@ App.Views.MapView = Backbone.View.extend({
     console.log("map view created")
 
     var mapOptions = {
-      zoom: 15,
+      zoom: 5,
       center: {
         lat: 40.7411,
         lng: -73.9897
@@ -14,15 +14,24 @@ App.Views.MapView = Backbone.View.extend({
 
     var map = new google.maps.Map(this.el, mapOptions);
 
+    var latitude = parseFloat(App.myEvents.models[0].attributes.latitude);
+    var latNum = eval(latitude);
+
+    var longitude = parseInt(App.myEvents.models[0].attributes.longitude);
+    var longNum = eval(longitude)
+
+    console.log(latNum);
+    //console.log(longitude);
+
     var marker = new google.maps.Marker({
     map:map,
     draggable:true,
     animation: google.maps.Animation.DROP,
-
-    position: { lat: 40.7411, lng: -73.9897 }
+    position: { lat: latNum, lng: longNum }
     });
-   
     marker.setMap(map)
+
+    //console.log(9898);
  }
 
 });
