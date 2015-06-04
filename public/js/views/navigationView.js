762d3f8c9bb5fed7d85a32bc440b0bc510dc865d
Michael Trumpf <miketrumpf@gmail.com>
2015-06-04 12:10:46 -0400

    fixed 2 bugs.  can now search more than once and fixed the list my events fetch error

--- public/js/views/navigationView.js
+++ public/js/views/navigationView.js
@@ -23,18 +23,25 @@ App.Views.Navigation = Backbone.View.extend({
   searchForEvents: function() {
     // var resultsDiv = document.getElementById("bar-results");
     // resultsDiv.removeAtributes("style");
+    $("#main").empty();
     $("#bar-results").css({
       "overflow": "scroll",
       "background-color": "white"
     });       
     $("#bar-results").empty(); 
 
+  // App.event = new App.Models.Event;
+  // App.events = new App.Collections.Events;
+  // App.eventsView = new App.Views.Events({collection: App.events});
     App.events.fetch();
   },
 
   seeMyEvents: function() {
     $("#main").empty();
     $("#bar-results").empty();
+
+    App.listEvents = new App.Collections.MyEvents;
+    App.listEventsView = new App.Views.ListEventsView({ collection: App.listEvents });
     App.listEvents.fetch();
   }
 
